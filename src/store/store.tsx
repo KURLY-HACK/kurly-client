a16ad17e184d19a66b15db7b7fb4d8aca665ce99
chaaerim <cofla411@ewhain.net>
2022-08-16 03:06:46 +0900

    feat : rtk setting

---
+++ src/store/store.tsx
@@ -0,0 +1,18 @@
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+export const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
