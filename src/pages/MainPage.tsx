20c3d099f7d130cd8ede81878957418aa74bdc58
chaaerim <cofla411@ewhain.net>
2022-08-22 22:37:04 +0900

    feat : 로그인 시 컬리뷰 추천 상품 렌더링

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,13 +3,22 @@ import Header from '../components/header/Header';
 import ProductLists from '../components/main/ProductLists';
 import styled from 'styled-components';
 import RecommendLists from '../components/main/RecommendLists';
+import { RootState, useAppSelector } from '../store/store';
 
 const MainPage = () => {
+  const isLoginned = useAppSelector((state: RootState) => state.login.success);
+
   return (
     <div>
       <Header />
-      <MainText>구독 중인 컬리뷰가 추천하는 상품</MainText>
-      <RecommendLists />
+      {isLoginned ? (
+        <>
+          <MainText>구독 중인 컬리뷰가 추천하는 상품</MainText>
+          <RecommendLists />
+        </>
+      ) : (
+        <MainText>로그인 후에 컬리뷰의 추천 상품을 확인해보세요!</MainText>
+      )}
       <MainText>신상품</MainText>
       <ProductLists />
     </div>
