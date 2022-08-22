24d5719bf99c8c20534e1b80445f8fa4650e70b3
juhyojeong <juhyojeong58@gmail.com>
2022-08-22 22:35:53 +0900

    chore: 필요없는 구문 제거

--- src/store/slices/subscribe/getSubscribeListSlice.tsx
+++ src/store/slices/subscribe/getSubscribeListSlice.tsx
@@ -11,7 +11,6 @@ export const getSubscribeListThunk = createAsyncThunk(
   'subscribe/getSubscribeList',
   async (thunkAPI) => {
     const response = await axios.get(`api/kurlyviews`);
-    console.log(response);
     return response.data;
   }
 );
