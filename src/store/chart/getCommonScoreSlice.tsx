ae5aa3e2f8c246a07b39195630a1fbb3e8b77d05
chaaerim <cofla411@ewhain.net>
2022-08-23 23:43:35 +0900

    chore : 가져오기 수정

--- src/store/chart/getCommonScoreSlice.tsx
+++ src/store/chart/getCommonScoreSlice.tsx
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { ICommonScore } from '../../../lib/interface';
+import { ICommonScore } from '../../lib/interface';
 
 const initialState: ICommonScore = {
   pending: false,
